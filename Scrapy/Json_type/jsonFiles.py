6db71012630a4bd9ee8fa49ba837683d25f63365
Knight <mayanks365@gmail.com>
2020-09-04 12:23:31 +0530

    scraping json pages

---
+++ Scrapy/Json_type/jsonFiles.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+class JsonfilesSpider(scrapy.Spider):
+    name = 'jsonFiles'
+    allowed_domains = ['trumptwitterarchive.com/']
+    start_urls = ['http://d5nxcu7vtzvay.cloudfront.net/data/realdonaldtrump/2017.json']
+
+    def parse(self, response):
+        resp = json.loads(response.body)
+
+        for tweet in resp:
+            yield{'source': tweet ['source'],
+                   'id_str': tweet['id_str'],
+                   'text': tweet['text'],
+                   'created_at': tweet['created_at'],
+                   'retweet_count':tweet ['retweet_count'],
+                   'in_reply_to_user_id_str':tweet['in_reply_to_user_id_str'],
+                   'favorite_count': tweet['favorite_count'],
+                   'is_retweet': tweet['is_retweet']
+                }
